2bd2182ede0d8828634790b397ec6b25e79986da
britton <brittonkfischer@gmail.com>
2022-05-22 15:25:34 -0700

    Fix Fuzz Testing

--- git-config/fuzz/fuzz_targets/parser.rs
+++ git-config/fuzz/fuzz_targets/parser.rs
@@ -1,9 +1,9 @@
 #![no_main]
 
-use git_config::parser::Parser;
+use git_config::parser::parse_from_bytes;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     // Don't name this _; Rust may optimize it out.
-    let _a = Parser::from_bytes(data);
+    let _a = parse_from_bytes(data);
 });
